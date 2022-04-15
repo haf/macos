b72862348bf3cb06c14ffa2d671dad22278afeeb
Henrik Feldt <henrik@haf.se>
2022-04-16 01:25:59 +0200

    Protobuf tooling

--- scripts/cli.sh
+++ scripts/cli.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
+brew tap bufbuild/buf
+brew tap argoproj/tap
+
 declare -a packages=(
 	"autoconf"
 	"autojump"       # https://github.com/wting/autojump
 	"automake"
+	"buf"
 	"bzip2"
 	"colordiff"
 	"curl"
@@ -23,6 +27,8 @@ declare -a packages=(
 	"openjdk@17"  # https://whichjdk.com/
 	"openssl"
 	"postgresql"  # https://stackoverflow.com/a/24645416 https://www.psycopg.org/psycopg3/docs/basic/install.html#local-installation
+	"protobuf"
+	"protoc-gen-grpc-web"
 	"pyenv"
 	"rbenv"
 	"readline"
@@ -42,3 +48,5 @@ for i in "${packages[@]}"
 do
    brew install "$i"
 done
+
+brew link --overwrite protobuf
